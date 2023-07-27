9eeb6d6a04941aafc1077946b6e273c5b07472fb
eyagueddes <eyagueddes1@gmail.com>
2023-07-27 18:43:59 +0100

    add spring security

---
+++ src/main/java/com/example/management/entities/AuthenticationBean.java
@@ -0,0 +1,26 @@
+package com.example.management.entities;
+
+
+public class AuthenticationBean {
+    private String message;
+
+    public AuthenticationBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Bienvenue dans backend [message=%s]",
+                message);
+
+    }
+
+}
